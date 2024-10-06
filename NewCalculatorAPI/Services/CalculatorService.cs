394bd9c71a9d0d82b4bf0b04310656db4109f01d
Parampreet30 <A00264414@mycambrian.ca>
2024-10-06 15:45:39 +0000

    ICalcultorService inherited

---
+++ NewCalculatorAPI/Services/CalculatorService.cs
@@ -0,0 +1,24 @@
+namespace NewCalculatorAPI.Inject;
+
+public class CalculatorService : ICalculatorService<decimal>
+{
+    public decimal Add(decimal left, decimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Divide(decimal left, decimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Multiply(decimal left, decimal right)
+    {
+        throw new NotImplementedException();
+    }
+
+    public decimal Subtract(decimal left, decimal right)
+    {
+        throw new NotImplementedException();
+    }
+}
