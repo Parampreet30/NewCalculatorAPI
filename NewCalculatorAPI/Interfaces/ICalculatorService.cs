40d2029bea24fe3a5d8b0f98c7c770be676683b1
Parampreet30 <A00264414@mycambrian.ca>
2024-10-06 15:40:12 +0000

    4 methods created

---
+++ NewCalculatorAPI/Interfaces/ICalculatorService.cs
@@ -0,0 +1,13 @@
+namespace NewCalculatorAPI.Inject;
+
+public interface ICalculatorService<T> {
+T Add(T left,T right);
+
+T Subtract(T left,T right);
+
+T Multiply(T left,T right);
+
+T Divide(T left,T right);
+
+
+}
